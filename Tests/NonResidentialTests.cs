0ab77fe994caf0369c2497efe7035189e61c7f9c
Airat Yusuff <airat.yusuff@bjss.com>
2024-10-18 15:54:33 +0100

    split up tests to make good use of setup method

---
+++ Tests/NonResidentialTests.cs
@@ -0,0 +1,32 @@
+﻿using week7_day4;
+
+namespace Tests
+{
+    public class NonResidentialTests
+    {
+        TaxCalculator calculator;
+        TaxBandsFactory TaxBands;
+        [SetUp]
+        public void Setup()
+        {
+            TaxBands = new NonResidentialTaxBands();
+            calculator = new TaxCalculator(TaxBands);
+        }
+
+        [Test]
+        public void Test_NonResidentialPropertyHasTaxBandA()
+        {
+            double taxPrice = calculator.CalculateTax(250000);
+
+            Assert.That(taxPrice, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void Test_NonResidentialPropertyHasHighestTaxBand()
+        {
+            double taxPrice = calculator.CalculateTax(465000);
+
+            Assert.That(taxPrice, Is.EqualTo(11750));
+        }
+    }
+}
--- Tests/ResidentialTests.cs
+++ Tests/ResidentialTests.cs
@@ -2,11 +2,15 @@ using week7_day4;
 
 namespace Tests
 {
-    public class Tests
+    public class ResidentialTests
     {
+        TaxCalculator calculator;
+        TaxBandsFactory TaxBands;
         [SetUp]
         public void Setup()
         {
+            TaxBands = new ResidentialTaxBands();
+            calculator = new TaxCalculator(TaxBands);
         }
 
         [Test]
@@ -21,7 +25,6 @@ namespace Tests
         [Test]
         public void Test_PropertyHasNoTax()
         {
-            var calculator = new TaxCalculator();
             double taxPrice = calculator.CalculateTax(130000);
 
             Assert.IsTrue(taxPrice == 0);
@@ -30,7 +33,6 @@ namespace Tests
         [Test]
         public void Test_PropertyHasTaxBandA()
         {
-            var calculator = new TaxCalculator();
             double taxPrice = calculator.CalculateTax(200000);
 
             Assert.That(taxPrice, Is.EqualTo(1100));
@@ -39,7 +41,6 @@ namespace Tests
         [Test]
         public void Test_PropertyHasTaxBandB()
         {
-            var calculator = new TaxCalculator();
             double taxPrice = calculator.CalculateTax(302000);
 
             Assert.That(taxPrice, Is.EqualTo(4700));
@@ -48,7 +49,6 @@ namespace Tests
         [Test]
         public void Test_PropertyHasTaxBandC()
         {
-            var calculator = new TaxCalculator();
             double taxPrice = calculator.CalculateTax(750000);
 
             Assert.That(taxPrice, Is.EqualTo(48350));
@@ -57,28 +57,9 @@ namespace Tests
         [Test]
         public void Test_PropertyHasTaxBandD()
         {
-            var calculator = new TaxCalculator();
             double taxPrice = calculator.CalculateTax(875000);
 
             Assert.That(taxPrice, Is.EqualTo(63350));
-        }
-
-        [Test]
-        public void Test_NonResidentialPropertyHasTaxBandA()
-        {
-            var calculator = new TaxCalculator(CalculatorTypes.NON_RESIDENTIAL);
-            double taxPrice = calculator.CalculateTax(250000);
-
-            Assert.That(taxPrice, Is.EqualTo(1000));
-        }
-
-        [Test]
-        public void Test_NonResidentialPropertyHasHighestTaxBand()
-        {
-            var calculator = new TaxCalculator(CalculatorTypes.NON_RESIDENTIAL);
-            double taxPrice = calculator.CalculateTax(465000);
-
-            Assert.That(taxPrice, Is.EqualTo(11750));
         }
     }
 }
